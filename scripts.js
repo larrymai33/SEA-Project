f68089f5bbee61094e1665975ad5396ac32549fd
Larry <larrymai33@gmail.com>
2026-04-16 20:31:35 -0700

    Display player info skeleton
    Base code to display all MLB player information, sucessfully shows all info in frontend and backend

--- scripts.js
+++ scripts.js
@@ -23,67 +23,83 @@
  *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
+const players = [
+  {
+    name: "Aaron Judge",
+    team: "New York Yankees",
+    position: "RF",
+    games: 158,
+    homeRuns: 58,
+    rbi: 144,
+    battingAvg: 0.322,
+    stolenBases: 10,
+    image: "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/592450/headshot/67/current"
+  },
+  {
+    name: "Shohei Ohtani",
+    team: "Los Angeles Dodgers",
+    position: "DH",
+    games: 159,
+    homeRuns: 44,
+    rbi: 96,
+    battingAvg: 0.310,
+    stolenBases: 59,
+    image: "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/660271/headshot/67/current"
+  },
+  {
+    name: "Bobby Witt Jr.",
+    team: "Kansas City Royals",
+    position: "SS",
+    games: 159,
+    homeRuns: 32,
+    rbi: 99,
+    battingAvg: 0.332,
+    stolenBases: 31,
+    image: "https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_213,q_auto:best/v1/people/677951/headshot/67/current"
+  }
 ];
 // Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
 
 // This function adds cards the page to display the data in the array
-function showCards() {
+function showCards(players) {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
+  for (let i = 0; i < players.length; i++) {
+    let player= players[i];
 
     // This part of the code doesn't scale very well! After you add your
     // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, player); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+function editCardContent(card, player) {
   card.style.display = "block";
 
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  card.querySelector("h2").textContent = player.name;
+  card.querySelector(".team").textContent = player.team;
+  card.querySelector(".position").textContent = player.position;
+  card.querySelector(".hr").textContent = "HR: " + player.homeRuns;
+  card.querySelector(".avg").textContent = "AVG: " +  player.battingAvg;
+  card.querySelector(".rbi").textContent = "RBI: " + player.rbi;
+  card.querySelector(".sb").textContent = "SB: " +  player.stolenBases;
 
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
-
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  const img = card.querySelector("img");
+  img.src = player.image;
+  img.alt = player.name + " headshot";
 }
 
 // This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
+document.addEventListener("DOMContentLoaded", function () {
+  showCards(players);
+});
 function quoteAlert() {
   console.log("Button Clicked!");
   alert(
